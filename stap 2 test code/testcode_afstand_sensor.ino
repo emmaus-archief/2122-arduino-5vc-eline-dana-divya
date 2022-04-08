79830dfab8aa199d705cdcfd14876b9587e938eb
ElineW05 <74536498+ElineW05@users.noreply.github.com>
2022-04-08 10:23:11 +0200

    goeie testcode van afstand sensor

---
+++ stap 2 test code/testcode_afstand_sensor.ino
@@ -0,0 +1,28 @@
+const int trigPin = 12;
+const int echoPin = 8;
+
+float duration;
+float distance;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration * 0.0343) / 2;
+
+  Serial.print("Afstand: ");
+  Serial.println(distance);
+
+  delay(100);
+}
