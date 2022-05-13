f2e3824fe46b162b15c183e8d286d299ce98a8fd
ElineW05 <74536498+ElineW05@users.noreply.github.com>
2022-05-13 10:10:08 +0200

    klein begin gemaakt, geen idee of t iets gaat doen

--- stap 4 project code/project_code_Dana_Divya_Eline.ino
+++ stap 4 project code/project_code_Dana_Divya_Eline.ino
@@ -1,11 +1,11 @@
 
 
-#define const int PWMA = 6; //Left Motor Speed pin (ENA)
-#define const int AIN2 = A0; //Motor-L forward (IN2).
-#define const int AIN1 = A1; //Motor-L backward (IN1)
-#define const int PWMB = 5; //Right Motor Speed pin (ENB)
-#define const int BIN1 = A2; //Motor-R forward (IN3)
-#define const int BIN2 = A3; //Motor-R backward (IN4)
+#define const int Lsnelheid = 6; //Left Motor Speed pin (ENA)
+#define const int Lvooruit = A0; //Motor-L forward (IN2).
+#define const int Lachteruit = A1; //Motor-L backward (IN1)
+#define const int Rsnelheid = 5; //Right Motor Speed pin (ENB)
+#define const int Rvooruit = A2; //Motor-R forward (IN3)
+#define const int Rachteruit = A3; //Motor-R backward (IN4)
 #define const int Clock = 13;
 #define const int Address = 12;
 #define const int DataOut = 11;
@@ -26,14 +26,22 @@ int TOESTAND = RIJDEN;
 
 void setup() {
   // put your setup code here, to run once:
-/* wat is output en wat is input */
+  pinMode(Lsnelheid, OUTPUT);
+  pinMode(Lvooruit, OUTPUT);
+  pinMode(Lachteruit OUTPUT);
+  pinMode(Rsnelheid, OUTPUT);
+  pinMode(Rvooruit, OUTPUT);
+  pinMode(Rachteruit, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
     if(TOESTAND == RIJDEN) {
-      
+      analogWrite(Lsnelheid, 100);
+      analogWrite(Lsnelheid, 100);
+      digitalWrite(Lvooruit, HIGH)
+      digitalWrit(Rvooruit, HIGH);
     }
 
     if(TOESTAND == REMMEN) {
