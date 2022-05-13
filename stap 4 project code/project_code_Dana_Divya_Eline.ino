411f29b6bcdd3668188aab99b2a9e39591ac5ec7
ElineW05 <74536498+ElineW05@users.noreply.github.com>
2022-05-13 10:28:14 +0200

    het werkt niet maar toch iets

--- stap 4 project code/project_code_Dana_Divya_Eline.ino
+++ stap 4 project code/project_code_Dana_Divya_Eline.ino
@@ -1,11 +1,11 @@
 
 
-#define const int Lsnelheid = 6; //Left Motor Speed pin (ENA)
-#define const int Lvooruit = A0; //Motor-L forward (IN2).
-#define const int Lachteruit = A1; //Motor-L backward (IN1)
-#define const int Rsnelheid = 5; //Right Motor Speed pin (ENB)
-#define const int Rvooruit = A2; //Motor-R forward (IN3)
-#define const int Rachteruit = A3; //Motor-R backward (IN4)
+#define PWMA   6           //Left Motor Speed pin (ENA)
+#define AIN2   A0          //Motor-L forward (IN2).
+#define AIN1   A1          //Motor-L backward (IN1)
+#define PWMB   5           //Right Motor Speed pin (ENB)
+#define BIN1   A2          //Motor-R forward (IN3)
+#define BIN2   A3          //Motor-R backward (IN4)
 #define const int Clock = 13;
 #define const int Address = 12;
 #define const int DataOut = 11;
@@ -19,32 +19,34 @@
 #define const int ECHO = 2;
 #define const int TRIG = 3;
 #define const int Addr = 0x20;
-const RIJDEN = 0;
-const REMMEN = 0;
-const DRAAIEN = 0;
+int RIJDEN = 0;
+int REMMEN = 0;
+int  DRAAIEN = 0;
 int TOESTAND = RIJDEN;
-
+  
 void setup() {
   // put your setup code here, to run once:
-  pinMode(Lsnelheid, OUTPUT);
-  pinMode(Lvooruit, OUTPUT);
-  pinMode(Lachteruit OUTPUT);
-  pinMode(Rsnelheid, OUTPUT);
-  pinMode(Rvooruit, OUTPUT);
-  pinMode(Rachteruit, OUTPUT);
-
+  pinMode(PWMA,OUTPUT);                     
+  pinMode(AIN2,OUTPUT);      
+  pinMode(AIN1,OUTPUT);
+  pinMode(PWMB,OUTPUT);       
+  pinMode(AIN1,OUTPUT);     
+  pinMode(AIN2,OUTPUT);  
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-    if(TOESTAND == RIJDEN) {
-      analogWrite(Lsnelheid, 100);
-      analogWrite(Lsnelheid, 100);
-      digitalWrite(Lvooruit, HIGH)
-      digitalWrit(Rvooruit, HIGH);
-    }
+if (TOESTAND == RIJDEN) {
+  analogWrite(PWMA,50);
+  digitalWrite(AIN1, LOW);
+  digitalWrite(AIN2,HIGH);
+  analogWrite(PWMB,50);
+  digitalWrite(BIN1, LOW);
+  digitalWrite(BIN2,HIGH);
+}
 
-    if(TOESTAND == REMMEN) {
+ if(TOESTAND == REMMEN) {
       
     }
 
