4e366b739c63de9077bec9a798baf0d07fd093cd
ElineW05 <74536498+ElineW05@users.noreply.github.com>
2022-05-19 16:25:36 +0200

    bijna werkende code

--- stap 4 project code/project_code_Dana_Divya_Eline.ino
+++ stap 4 project code/project_code_Dana_Divya_Eline.ino
@@ -1,4 +1,4 @@
-
+#include <Wire.h>
 const int PWMA = 6;           //Left Motor Speed pin (ENA)
 const int AIN2 = A0;         //Motor-L forward (IN2).
 const int AIN1 = A1;          //Motor-L backward (IN1)
@@ -20,38 +20,98 @@ const int TRIG = 3;
 const int Addr = 0x20;
 int RIJDEN = 0;
 int REMMEN = 1;
-int  DRAAIEN = 2;
-int TOESTAND = RIJDEN;
-int speed = 20;
-  
+int DRAAIEN = 2;
+int TOESTAND = DRAAIEN;
+int snelheid = 75;
+
+void PCF8574Write(byte data);
+byte PCF8574Read();
+
 void setup() {
   // put your setup code here, to run once:
-   
-  
+  Serial.begin(115200);
+  Wire.begin();
+  snelheid = 75;
+  analogWrite(PWMA, snelheid);
+  analogWrite(PWMB, snelheid);
+  pinMode(PWMA, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  pinMode(AIN1, OUTPUT);
+  pinMode(PWMB, OUTPUT);
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-bool muurGedetecteerd = false;
-if (TOESTAND == RIJDEN) {
-  /* zorgen dat hij rechtdoor rijdt */
+  bool muurGedetecteerd = false;
+  if (TOESTAND == RIJDEN) {
+    Serial.println("RIJDEN");
+    snelheid = 75;
+    analogWrite(PWMA, snelheid);
+    analogWrite(PWMB, snelheid);
+    digitalWrite(AIN1, LOW);
+    digitalWrite(AIN2, HIGH);
+    digitalWrite(BIN1, LOW);
+    digitalWrite(BIN2, HIGH);
     if (muurGedetecteerd = true) {
-    TOESTAND == REMMEN;
+      TOESTAND == REMMEN;
     }
-}
+  }
 
-if(TOESTAND == REMMEN) {
-  /* speed verlagen */
-    if (speed = 0) {
-    TOESTAND == DRAAIEN;
+  if (TOESTAND == REMMEN) {
+    Serial.println("REMMEN");
+    stilstaan();
+    if (snelheid = 0) {
+      TOESTAND == DRAAIEN;
+    }
+  }
+
+  if (TOESTAND == DRAAIEN) {
+    draaien();
+    if (muurGedetecteerd == false) {
+      TOESTAND == RIJDEN;
+    }
   }
 }
 
-if(TOESTAND == DRAAIEN) {
-  /* draaien naar links of rechts */
-    if (muurGedetecteerd = false) {
-    TOESTAND == RIJDEN;
-    }
+void stilstaan() {
+  snelheid = 0;
+  analogWrite(PWMA, snelheid);
+  analogWrite(PWMB, snelheid);
+  digitalWrite(AIN1, LOW);
+  digitalWrite(BIN1, LOW);
+  digitalWrite(AIN2, LOW);
+  digitalWrite(BIN2, LOW);
+}
+
+void draaien() {
+  snelheid = 50;
+  analogWrite(PWMA, snelheid);
+  analogWrite(PWMB, snelheid);
+  digitalWrite(AIN1, HIGH);
+  digitalWrite(BIN2, HIGH);
+  digitalWrite(AIN2, LOW);
+  digitalWrite(BIN1, LOW);
+}
+
+
+void PCF8574Write(byte data)
+{
+  Wire.beginTransmission(Addr);
+  Wire.write(data);
+  Wire.endTransmission(); 
+}
+
+byte PCF8574Read()
+{
+  int data = -1;
+  Wire.requestFrom(Addr, 1);
+  if(Wire.available()) {
+    data = Wire.read();
   }
-  
+  return data;
+}
+
+void muurGedetecteerd() {
+
 }
